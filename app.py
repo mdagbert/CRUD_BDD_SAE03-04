396c942ec1a2bda6d543f72ed1988546811d541e
mdagbert <mathis.dagbert@edu.univ-fcomte.fr>
2024-11-26 09:49:28 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import copy
+
+from flask import Flask, request, render_template, redirect, url_for, abort, flash
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.secret_key = 'une cle(token) : grain de sel(any random string)'
+
+
+@app.route('/')
+def show_accueil():
+    return render_template('layout.html')
+
+
+if __name__ == '__main__':
+    app.run()
+
+
+def generate_flash_message(flashName, flashList):
+    message = f'{flashName},<br>'
+    for key in flashList.keys():
+        if key == 'Modifier' or key == "Valider":  # ne pas modifier
+            break
+        value = flashList.getlist(key)[0]
+        message += f'{key}: {value}<br>'
+
+    flash(message, 'alert-success')
